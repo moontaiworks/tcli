3a1fb1bc3023377466628a77e3e6ffab51d5bd96
moontai0724 <moontai0724@gmail.com>
2025-04-25 14:06:50 +0800

    test: global import vitest

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,3 @@
-import { describe, expect, it } from "vitest";
-
 describe("integration test", () => {
   it("should be able to execute", async () => {
     await expect(import("@/index")).resolves.toBeDefined();
--- vitest.config.ts
+++ vitest.config.ts
@@ -10,17 +10,21 @@ export default defineConfig({
     },
   },
   test: {
+    clearMocks: true,
     coverage: {
       enabled: true,
       include: ["src/**/*.ts"],
       reporter: ["text", "text-summary", "json", "html", "cobertura"],
       reportOnFailure: true,
     },
+    globals: true,
+    mockReset: true,
     outputFile: {
       junit: "./coverage/junit-report.xml",
     },
     passWithNoTests: true,
     reporters: ["default", "junit"],
+    restoreMocks: true,
     typecheck: {
       checker: "tsc",
       tsconfig: "./tests/tsconfig.json",
