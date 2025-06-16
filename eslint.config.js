2f7ad7ae66caba21a1589ff959d2cae46141ec58
moontai0724 <moontai0724@gmail.com>
2025-06-16 13:04:19 +0800

    build: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,21 @@
-// @ts-check
 import pluginJs from "@eslint/js";
 import tsParser from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginImportX from "eslint-plugin-import-x";
+import { flatConfigs as eslintPluginImportX } from "eslint-plugin-import-x";
 import pluginNode from "eslint-plugin-n";
 import perfectionist from "eslint-plugin-perfectionist";
-import { configs as tsConfigs, config as typedConfig } from "typescript-eslint";
+import {
+  config as extendConfig,
+  configs as tsConfigs,
+} from "typescript-eslint";
 
 export default [
   { ignores: ["node_modules", "dist", "build", "docs", "coverage"] },
   pluginJs.configs.recommended,
+  pluginNode.configs["flat/recommended-module"],
+  eslintPluginImportX.recommended,
+  eslintPluginImportX.typescript,
+  perfectionist.configs["recommended-natural"],
   {
     files: ["**/*.ts"],
     languageOptions: {
@@ -24,34 +30,6 @@ export default [
       },
     },
   },
-  eslintPluginImportX.flatConfigs.recommended,
-  eslintPluginImportX.flatConfigs.typescript,
-  ...typedConfig({
-    extends: [
-      ...tsConfigs.strictTypeChecked,
-      ...tsConfigs.stylisticTypeChecked,
-    ],
-    files: ["src/**/*.ts", "tests/**/*.ts"],
-  }),
-  ...typedConfig({
-    extends: [perfectionist.configs["recommended-natural"]],
-    files: ["src/**/*.ts", "tests/**/*.ts"],
-    rules: {
-      "perfectionist/sort-imports": [
-        "error",
-        {
-          groups: [
-            ["builtin", "external"],
-            "internal",
-            ["parent", "sibling", "index"],
-            ["object", "unknown"],
-          ],
-          internalPattern: ["~/**", "@/**"],
-        },
-      ],
-    },
-  }),
-  pluginNode.configs["flat/recommended-module"],
   {
     rules: {
       "n/no-missing-import": [
@@ -60,7 +38,50 @@ export default [
           allowModules: ["eslint-config-prettier"],
         },
       ],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+            ["object", "unknown"],
+          ],
+          internalPattern: [/^([~@]\/|#)/.source],
+        },
+      ],
     },
   },
+  ...extendConfig({
+    extends: [
+      ...tsConfigs.strictTypeChecked,
+      ...tsConfigs.stylisticTypeChecked,
+    ],
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { disallowTypeAnnotations: false },
+      ],
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        { allowNumber: true },
+      ],
+    },
+    settings: {
+      node: {
+        tryExtensions: [".ts", ".d.ts", "/index.ts", "/index.d.ts"],
+      },
+    },
+  }),
   eslintConfigPrettier,
 ];
